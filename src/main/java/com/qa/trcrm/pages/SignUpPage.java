39cb2fef47f80e2d89e789cd7cd73fc3dcf23b4e
sachinsharma263 <sachinsharma263@gmail.com>
2023-06-23 00:25:15 +0530

    updated

--- src/main/java/com/qa/trcrm/pages/SignUpPage.java
+++ src/main/java/com/qa/trcrm/pages/SignUpPage.java
@@ -9,7 +9,13 @@ public class SignUpPage {
 	public SignUpPage(WebDriver driver) {
 		this.driver = driver;
 	}
+
 	public void m2() {
 		System.out.println("m2--remote");
+	}
+
+	public void m1() {
+		System.out.println("m1--local");
+
 	}
 }
