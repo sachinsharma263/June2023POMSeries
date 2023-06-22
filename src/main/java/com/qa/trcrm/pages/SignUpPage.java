13335882df2a34656ee9d55a1700910565471d3b
sachinsharma263 <sachinsharma263@gmail.com>
2023-06-22 23:43:13 +0530

    SignUpPage added

---
+++ src/main/java/com/qa/trcrm/pages/SignUpPage.java
@@ -0,0 +1,12 @@
+package com.qa.trcrm.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class SignUpPage {
+
+	WebDriver driver;
+
+	public SignUpPage(WebDriver driver) {
+		this.driver = driver;
+	}
+}
