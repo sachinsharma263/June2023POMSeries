5853a2beb929a6b896aaef8bf40578aabe14004e
sachinsharma263 <sachinsharma263@gmail.com>
2023-06-24 12:26:50 +0530

    appconstant

--- src/main/java/com/qa/trcrm/utils/AppConstants.java
+++ src/main/java/com/qa/trcrm/utils/AppConstants.java
@@ -2,14 +2,17 @@ package com.qa.trcrm.utils;
 
 public class AppConstants {
 
-	public static final String LOGIN_PAGE_TITLE="CRM Login CRM | Customer Relationship Management TRCRM";
-	public final static String HOME_PAGE_TITLE="TRCRM";
-	public final static String HOME_PAGE_HEADER="Homepage";
-	
-	public final static int DEFAULT_TIME_OUT=20;
+	public static final String LOGIN_PAGE_TITLE = "CRM Login CRM | Customer Relationship Management TRCRM";
+	public final static String HOME_PAGE_TITLE = "TRCRM";
+	public final static String HOME_PAGE_HEADER = "Homepage";
+
+	public final static int DEFAULT_TIME_OUT = 20;
 	public static final String TEST_SHEET_DATA = "./src/main/java/com/qa/trcrm/testdata/TestData.xlsx";
-	
-	public static final String PERSON_ADDED_MSG="Person added.";
-	
-	public final static String CONTACT_PAGE_HEADER="Contacts";
+
+	public static final String PERSON_ADDED_MSG = "Person added.";
+
+	public final static String CONTACT_PAGE_HEADER = "Contacts";
+	public final static String CONTACT_PAGE_TITLE = "TRCRM";
+
+	public static final String CONTACTS_SHEET_NAME = "contacts";
 }
